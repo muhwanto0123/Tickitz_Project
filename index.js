3b676e88278ca3af840e77c68257ca1c185d7c37
muhwanto0123 <55321001+muhwanto0123@users.noreply.github.com>
2023-10-09 07:40:51 +0700

    [ADD]

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 require("dotenv").config()
 const express = require("express") //method express
-const app = express()
-const port = process.env.CLIENT_PORT
 const cors = require("cors")
 const helmet = require("helmet")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+
+
+const app = express()
+const port = process.env.CLIENT_PORT
 
 const moviesRouter = require('./routers/movies')
+const usersRouter = require('./routers/users');
+
 
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+// app.use(
+//     cors({
+//         origin: "*",
+//         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     })
+// )
 
+// app.use(helmet())
 app.use(moviesRouter)
+app.use(usersRouter)
 
 
 
--- routers/movies.js
+++ routers/movies.js
@@ -1,11 +1,10 @@
 const router = require("express").Router()
-const moviesController = require("../controller/movies")
 const moviescontroller = require("../controller/movies")
 
 router.get("/movies", moviescontroller.getAllMovies_Controller)
-router.get("/movies/:id", moviesController.getIDMovies_Controller)
+router.get("/movies/:id", moviescontroller.getIDMovies_Controller)
 router.post("/movies", moviescontroller.postAddMovies_Controller)
-router.put("/movies/:id", moviesController.putUpdateMovies_Controller)
-router.delete("/movies/:id", moviesController.deteledSelectedMovies_Controller)
+router.put("/movies/:id", moviescontroller.putUpdateMovies_Controller)
+router.delete("/movies/:id", moviescontroller.deteledSelectedMovies_Controller)
 
 module.exports = router
