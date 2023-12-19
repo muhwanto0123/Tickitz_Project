e90a7ba5af38f0afb63bef6ccd9ccc952c81a179
muhwanto0123 <55321001+muhwanto0123@users.noreply.github.com>
2023-12-19 16:08:46 +0700

    [update]

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-require("dotenv").config()
-const express = require("express") //method express
+require("dotenv").config();
+const express = require("express"); //method express
 // const cors = require("cors")
 // const helmet = require("helmet")
 // const bcrypt = require("bcrypt")
 // const jwt = require("jsonwebtoken")
 
+const app = express();
+const port = process.env.CLIENT_PORT;
 
-const app = express()
-const port = process.env.CLIENT_PORT
+const moviesRouter = require("./src/routers/movies");
+const usersRouter = require("./src/routers/users");
+const cinemaRouter = require("./src/routers/cinemas");
 
-const moviesRouter = require('./src/routers/movies')
-const usersRouter = require('./src/routers/users');
-const cinemaRouter = require('./src/routers/cinemas')
-
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 // app.use(
 //     cors({
 //         origin: "*",
@@ -23,12 +22,14 @@ app.use(express.json())
 // )
 
 // app.use(helmet())
-app.use(moviesRouter)
-app.use(usersRouter)
-app.use(cinemaRouter)
+app.use(moviesRouter);
+app.use(usersRouter);
+app.use(cinemaRouter);
 
-
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(port, () => {
-    console.log(`http://localhost:${port}`)
-})
+  console.log(`http://localhost:${port}`);
+});
