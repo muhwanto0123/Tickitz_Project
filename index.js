fcf6cce0147d678feb6fc4fa074fdfb2df1529a8
muhwanto0123 <55321001+muhwanto0123@users.noreply.github.com>
2023-12-19 16:20:26 +0700

    [update]

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const cinemaRouter = require("./src/routers/cinemas");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 app.use(
     cors({
         origin: "*",
@@ -22,6 +23,7 @@ app.use(
 )
 
 app.use(helmet())
+
 app.use(moviesRouter);
 app.use(usersRouter);
 app.use(cinemaRouter);
